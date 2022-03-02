cbf14384bab4f277ec207740d49ae353501b7c71
bhorlarh <bhorlarinwah@gmail.com>
2022-03-02 08:53:16 +0100

    wip - 23: io and sorting.

---
+++ io/file_system_store.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"encoding/json"
+	"os"
+)
+
+type FileSystemPlayerStore struct {
+	database *json.Encoder
+	league   League
+}
+
+func (f *FileSystemPlayerStore) GetLeague() League {
+	return f.league
+}
+
+func (f *FileSystemPlayerStore) GetPlayerScore(name string) int {
+	player := f.league.Find(name)
+
+	if player != nil {
+		return player.Wins
+	}
+
+	return 0
+}
+
+func (f *FileSystemPlayerStore) RecordWin(name string) {
+	league := f.GetLeague()
+	player := league.Find(name)
+
+	if player != nil {
+		player.Wins++
+	} else {
+		f.league = append(league, Player{name, 1})
+	}
+
+	f.database.Encode(league)
+}
+
+func NewFileSystemPlayerStore(database *os.File) *FileSystemPlayerStore {
+	database.Seek(0, 0)
+	league, _ := NewLeague(database)
+
+	return &FileSystemPlayerStore{
+		database: json.NewEncoder(&tape{database}),
+		league:   league,
+	}
+
+}
--- json/main.go
+++ json/main.go
@@ -5,30 +5,6 @@ import (
 	"net/http"
 )
 
-func NewInMemoryPlayerStore() *InMemoryPlayerStore {
-	return &InMemoryPlayerStore{map[string]int{}}
-}
-
-type InMemoryPlayerStore struct {
-	store map[string]int
-}
-
-func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
-	return i.store[name]
-}
-
-func (i *InMemoryPlayerStore) RecordWin(name string) {
-	i.store[name]++
-}
-
-func (i *InMemoryPlayerStore) GetLeague() []Player {
-	var league []Player
-	for name, wins := range i.store {
-		league = append(league, Player{name, wins})
-	}
-	return league
-}
-
 func main() {
 	server := NewPlayerServer(NewInMemoryPlayerStore())
 	log.Fatal(http.ListenAndServe(":5000", server))
