ae74f05a1648ca11f92c6d79b87f7170b9c65ee0
bhorlarh <bhorlarinwah@gmail.com>
2022-03-06 16:32:42 +0100

    wip: attempting refactor. 25: time.

---
+++ time/CLI.go
@@ -0,0 +1,59 @@
+package poker
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"strconv"
+	"strings"
+	"time"
+)
+
+const PlayerPrompt = "Please enter the number of players: "
+
+type CLI struct {
+	playerStore PlayerStore
+	in          *bufio.Scanner
+	out         io.Writer
+	alerter     BlindAlerter
+}
+
+func (c *CLI) PlayPoker() {
+	fmt.Fprint(c.out, PlayerPrompt)
+
+	numberOfPlayers, _ := strconv.Atoi(c.readLine())
+
+	c.scheduleBlindAlerts(numberOfPlayers)
+
+	userInput := c.readLine()
+	c.playerStore.RecordWin(extractWinner(userInput))
+}
+
+func (c *CLI) scheduleBlindAlerts(numberOfPlayers int) {
+	blindIncrement := time.Duration(5+numberOfPlayers) * time.Minute
+
+	blinds := []int{100, 200, 300, 400, 500, 600, 800, 1000, 2000, 4000, 8000}
+	blindTime := 0 * time.Second
+	for _, blind := range blinds {
+		c.alerter.ScheduleAlert(blindTime, blind)
+		blindTime = blindTime + blindIncrement
+	}
+}
+
+func extractWinner(userInput string) string {
+	return strings.Replace(userInput, " wins", "", 1)
+}
+
+func (c *CLI) readLine() string {
+	c.in.Scan()
+	return c.in.Text()
+}
+
+func NewCLI(store PlayerStore, in io.Reader, out io.Writer, alerter BlindAlerter) *CLI {
+	return &CLI{
+		playerStore: store,
+		in:          bufio.NewScanner(in),
+		out:         out,
+		alerter:     alerter,
+	}
+}
---
+++ time/cmd/cli/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	poker "github.com/rinwaowuogba/learn-go-with-tests/time"
+)
+
+const dbFileName = "game.db.json"
+
+func main() {
+	fmt.Println("Let's play poker")
+	fmt.Println("Type {name} wins to record a win")
+
+	store, close, err := poker.FileSystemPlayerStoreFromFile(dbFileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer close()
+
+	game := poker.NewCLI(store, os.Stdin, poker.BlindAlerterFunc(poker.StdOutAlerter))
+	game.PlayPoker()
+}
